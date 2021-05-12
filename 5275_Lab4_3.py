d6a61076bbbf6407126ac5d6ee25e977f577d2bf
Paul Yeh <Paul Yeh>
2021-05-12 22:50:50 +0800

    Finish Problem 1

--- 5275_Lab4_3.py
+++ 5275_Lab4_3.py
@@ -18,27 +18,16 @@ def split_train_valid_set(x_train, y_train, **kwargs):
     return x_train[:n_train], y_train[:n_train], x_train[n_train:], y_train[n_train:]
 
 
-def load_data(data_dir, subject, batch_size=32, split=False, valid_split_rate=0):
+def load_data(data_path, subject, dev, batch_size=32):
     
-    train = io.loadmat(data_dir + 'BCIC_' + subject + '_T.mat')
-    test = io.loadmat(data_dir + 'BCIC_' + subject + '_E.mat')
+    train = io.loadmat(data_path + 'BCIC_' + subject + '_T.mat')
+    test = io.loadmat(data_path + 'BCIC_' + subject + '_E.mat')
 
-    x_train = torch.Tensor(train['x_train']).unsqueeze(1) # Shape: [288, 1, 22, 562]
-    y_train = torch.Tensor(train['y_train']).view(-1) # Shape [288]
-    x_test = torch.Tensor(test['x_test']).unsqueeze(1)
-    y_test = torch.Tensor(test['y_test']).view(-1)
+    x_train = torch.Tensor(train['x_train']).unsqueeze(1).to(dev) # Shape: [288, 1, 22, 562]
+    y_train = torch.Tensor(train['y_train']).view(-1).long().to(dev) # Shape [288]
+    x_test = torch.Tensor(test['x_test']).unsqueeze(1).to(dev)
+    y_test = torch.Tensor(test['y_test']).view(-1).to(dev)
 
-    if split:
-        if valid_split_rate < 0 or valid_split_rate >= 1:
-            raise ValueError('Unexpected Valid_split_rate.')
-        x_train, y_train, x_valid, y_valid = split_train_valid_set(x_train, y_train, valid_split_rate)
-        valid_dataset = Data.TensorDataset(x_valid, y_valid)
-        validloader = Data.DataLoader(
-            dataset = valid_dataset,
-            batch_size = 1,
-            shuffle = False,
-            num_workers = 0,
-        )
 
     train_dataset = Data.TensorDataset(x_train, y_train)
     test_dataset = Data.TensorDataset(x_test, y_test)
@@ -57,4 +46,16 @@ def load_data(data_dir, subject, batch_size=32, split=False, valid_split_rate=0)
         num_workers = 0,
     )
 
-    return trainloader, validloader, testloader
+    return trainloader, testloader
+
+def print_EEGNet_metric(acc, clf_acc, conf_m):
+    print('Accuracy of EEGNet: {:5f}'.format(acc))
+    print('-----------------------------------')
+    print('Overall Accuracy:\nLabel\tAccuracy')
+    for label, acc in zip(range(len(clf_acc)), clf_acc):
+        print('{}\t{:5f}'.format(label, acc))
+    print('-----------------------------------')
+    print('confusion matrix (prediction by Ground Truth):')
+    for i in range(len(conf_m)):
+        row_i = conf_m[i]
+        print('{} {} {} {}'.format(row_i[0], row_i[1], row_i[2], row_i[3]))
